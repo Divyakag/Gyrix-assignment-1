6110b821e28e294ea55069ee8b67d54b24e2aa83
Divyakag <48755818+Divyakag@users.noreply.github.com>
2020-10-29 15:37:00 +0530

    Update current-time.php

--- current-time.php
+++ current-time.php
@@ -1,38 +1,40 @@
 <?php
-function currentTime($country){
+function currentTime($country)// create function
+    //start function body
+{
     $IN="india";
     $NZ="newzealand";
     $Aus="australia";
     $Ger="germany";
     $USA="america";
 
-    if($country==$IN)
+    if($country==$IN)// check if parameter is equal to variable print india current time
     {
         date_default_timezone_set("IN");
         echo "<p style='color:red'>Current  time in India is </p>".  date("h:i:sa");
     }
-    else if($country==$NZ)
+    else if($country==$NZ)//newzealand
     {
         date_default_timezone_set("NZ");
         echo "<p style='color:green'>Current time in Newzealand is </p>" . date("h:i:sa");
     }
-    else if($country==$Aus)
+    else if($country==$Aus)//australia
     {
         date_default_timezone_set("Australia/Sydney");
         echo "<p style='color:blue'>Current time in Australia is </p> " . date("h:i:sa");
     }
-    else if($country==$USA)
+    else if($country==$USA)//america
     {
         date_default_timezone_set("America/New_York");
         echo "<p style='color: red'>Current time in America is </p>" . date("h:i:sa");
     }
-    else if($country==$Ger)
+    else if($country==$Ger)//germany
     {
         date_default_timezone_set("IST");
         echo "<p style='color: yellow'>Current time in Germany is </p> " . date("h:i:sa");
     }
 
 }
-currentTime("germany");
+currentTime("india");
 
 ?>
